692f815fc303d5660ad2f40fec368148b3af4ec3
Colin Alworth <niloc132@gmail.com>
2011-03-09 17:30:59 -0600

    Cleaned up a few files, made the html file actually load something, and added a crap-ton of widgets and entities. Widgets won't work without celltable-tools, but they arent required for a compile anyway, at least not yet.

---
+++ tvguide-client/src/main/java/com/acme/gwt/client/widget/EditableEpisodeListWidget.java
@@ -0,0 +1,57 @@
+/**
+ *  Copyright 2011 Colin Alworth
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.acme.gwt.client.widget;
+
+import com.acme.gwt.shared.EpisodeProxy;
+import com.colinalworth.celltable.columns.client.Columns;
+import com.colinalworth.celltable.columns.client.HasDataFlushableEditor;
+import com.google.gwt.cell.client.EditTextCell;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.editor.client.Editor.Path;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * @author colin
+ *
+ */
+public class EditableEpisodeListWidget extends Composite {
+	private static Binder uiBinder = GWT.create(Binder.class);
+
+	interface Binder extends UiBinder<Widget, EditableEpisodeListWidget> {}
+
+	interface EpisodeColumns extends Columns<EpisodeProxy> {
+		EditTextCell season();//erp int != String
+		EditTextCell episodeNumber();// int != String
+
+		EditTextCell name();
+	}
+	private EpisodeColumns columns = GWT.create(EpisodeColumns.class);
+	@Path("") HasDataFlushableEditor<EpisodeProxy> listEd;
+	@UiField(provided=true) CellTable<EpisodeProxy> list = new CellTable<EpisodeProxy>();
+	public EditableEpisodeListWidget() {
+		listEd = HasDataFlushableEditor.of(list);
+
+		columns.configure(list, listEd);
+
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+}
---
+++ tvguide-client/src/main/java/com/acme/gwt/client/widget/FavoriteShowsListWidget.java
@@ -0,0 +1,63 @@
+/**
+ *  Copyright 2011 Colin Alworth
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.acme.gwt.client.widget;
+
+import java.util.List;
+
+import com.acme.gwt.shared.ShowProxy;
+import com.colinalworth.celltable.columns.client.Columns;
+import com.colinalworth.celltable.columns.client.HasDataFlushableEditor;
+import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.editor.client.Editor;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * @TODO consider pushing the basics of this to a shows list widget, and wrapping it for use for
+ * Favorites
+ * 
+ * @author colin
+ *
+ */
+public class FavoriteShowsListWidget extends Composite implements Editor<List<ShowProxy>> {
+	private static Binder uiBinder = GWT.create(Binder.class);
+
+	interface Binder extends UiBinder<Widget, FavoriteShowsListWidget> {}
+
+	interface ShowColumns extends Columns<ShowProxy> {
+		TextCell name();
+		TextCell description();
+	}
+
+	private ShowColumns columns = GWT.create(ShowColumns.class);
+	@Path("") HasDataFlushableEditor<ShowProxy> listEditor;
+	@UiField(provided=true) CellTable<ShowProxy> list = new CellTable<ShowProxy>();
+
+	public FavoriteShowsListWidget() {
+		listEditor = HasDataFlushableEditor.of(list);
+
+		columns.configure(list, listEditor);
+
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+
+}
